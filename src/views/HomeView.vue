30bd9344140fbda1be72fba8dfc749c9e6d795a7
Nuradilaagustina <76506177+Nuradilaagustina@users.noreply.github.com>
2022-05-10 23:04:14 +0700

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,59 @@
+<script setup>
+import { onMounted } from "vue";
+
+onMounted(() => {
+  document.getElementsByTagName("body")[0].style.backgroundImage ="Url('src/assets/Kareem.png')"
+  document.getElementsByTagName("body")[0].style.backgroundSize ="cover";
+});
+
+</script>
+
+<template>
+  <main>
+
+    <div class="card bg-dark text-white">
+
+      <div class="card-img-overlay">
+        <h5 class="atas">Welcome to my web</h5>
+        <p class="card-text">"Bacalah Al-quran, karna Al-quran akan datang pada hari kiamat nanti sebagai syafi' (pemberi syafa'at) bagi yang membacanya"</p>
+        <p class="text1">(HR.Muslim)</p>
+        <p class="lead">Ini adalah website yang berisikan beberapa ayat alquran dengan API dari Quran.com untuk pemenuhan tugas UTS Pemrogaman Web</p>
+        <p class="akses"></p>
+        <p>Pelajari lebih lanjut tentang API Quran.com</p>
+        <a class="btn btn-dark btn-lg" href="#" role="button"> <a href="https://quran.api-docs.io/v4/getting-started/introduction" class="text-white fw-bold">Quran.Api</a></a>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style>
+.card-img-overlay{
+  margin-top: 90px;
+  text-align: center;
+  color: #0a0909;
+}
+
+.card-text{
+  margin-top: 40px;
+  font-size: 18px;
+  font-weight: bold;
+
+}
+.atas{
+  font-size: 55px;
+  font-family: "Blackadder ITC";
+  font-weight: bold;
+}
+.akses{
+  margin-top: 20px;
+}
+.lead{
+  margin-top: 50px;
+  font-weight: bold;
+  background-color: #c8946b;
+}
+.text1{
+  font-size: 18px;
+  font-weight: bold;
+}
+</style>
---
+++ src/views/JuzView.vue
@@ -0,0 +1,83 @@
+<template>
+  <section class="semua">
+    <section class="page-section" id="team">
+      <div class="container">
+        <div class="text-center">
+        <h3 class="find">You can find me on </h3>
+        </div>
+        <div class="row">
+          <div class="col-lg-1">
+            <div class="team-member">
+              <a class="btn btn-social  mx-2" style="background-image: url('src/assets/fb1.png');" href="https://www.facebook.com/dila.nuradila.5"></a>
+
+            </div>
+          </div>
+          <div class="col-lg-1">
+            <div class="team-member">
+              <a class="btn btn-social mx-2" style="background-image: url('src/assets/gitt.jpg');" href="https://github.com/Nuradilaagustina"></a>
+
+            </div>
+          </div>
+          <div class="col-lg-1">
+            <div class="team-member">
+                <a class="btn btn-social mx-2" style="background-image: url('src/assets/ige.png');" href="https://www.instagram.com/nuradilaagtna/?hl=id="></a>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </section>
+</template>
+
+<script>
+import { onMounted } from "vue";
+
+onMounted(() => {
+  document.getElementsByTagName("body")[0].style.backgroundColor = "#E1BA9D";
+  document.getElementsByTagName("body")[0].style.backgroundSize ="cover";
+});
+
+</script>
+
+<style scoped>
+.team-member {
+  margin-left: 32rem;
+
+}
+.team-member img {
+  width: 14rem;
+  height: 14rem;
+  border: 0.5rem solid rgba(0, 0, 0, 0.1);
+
+}
+.team-member h4, .team-member {
+  margin-top: 10rem;
+  margin-bottom: 0;
+}
+
+.page-section {
+  background-size: cover;
+  background-repeat: no-repeat;
+
+}
+.find{
+  font-family: "Segoe UI";
+  font-size: 40px;
+  color: #727667;
+  font-weight: bold;
+  padding-bottom: 40px;
+  margin-top: 90px;
+}
+
+.btn-social {
+  height: 2.5rem;
+  width: 2.5rem;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0;
+  background-size: contain;
+  background-repeat: no-repeat;
+}
+</style>
